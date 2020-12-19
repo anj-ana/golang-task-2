0db0ab3c1036810a290d598397c7d9cd172cd852
Anjana <66191342+anj-ana@users.noreply.github.com>
2020-12-19 11:20:46 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Anjana"
+	id := 123
+	var mark float32 = 23.2
+	fmt.Printf("my name is %s my id is %d and m mark is %.2f", name, id, mark)
+
+}
